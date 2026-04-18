fb3ecb3f2b41d31f66f06e25668c0465f8b83617
NicoleGaddis <daniellegaddis@mail.weber.edu>
2026-04-17 21:23:59 -0600

    Added link to home page when customer logs in to create a reservation when user enters the dates they want and the type of site they need the filtered list of available sites is shown Next to each avaible site the user can select to reserve the site

--- SiteReservationSystem.Web/SiteReservationSystem.Web/Controllers/HomeController.cs
+++ SiteReservationSystem.Web/SiteReservationSystem.Web/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SiteReservationSystem.Web.Data;
 using SiteReservationSystem.Web.Models;
+using SiteReservationSystem.Web.ViewModels;
 using System.Diagnostics;
 
 namespace SiteReservationSystem.Web.Controllers
 {
     public class HomeController : BaseController
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             // Redirect to login page if user is not logged in. This is a function in BaseController.cs
@@ -32,5 +42,96 @@ namespace SiteReservationSystem.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public async Task<IActionResult> CreateReservation()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Search(DateTime startDate, DateTime endDate, int? filterSite, int? length)
+        {
+            var query = _context.Sites.Include(s => s.SitePhotos).AsQueryable();
+
+            // Filter by type
+            query = query.Where(s => s.SiteTypeID == filterSite);
+
+            // Filter by length only if it's provided (Bring own Unit)
+            if (filterSite == 1 && length.HasValue)
+            {
+                query = query.Where(s => s.MaxLengthFeet > length.Value);
+            }
+
+            // 2. Filter out sites that have any overlapping reservations
+            // Overlap logic: (StartA < EndB) AND (EndA > StartB)
+            var availableSites = await query
+                .Where(site => !site.Reservations.Any(r =>
+                    startDate < r.EndDate && endDate > r.StartDate))
+                .ToListAsync();
+            
+            return View("CreateReservation", availableSites);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Reserve(int siteId, DateTime startDate, DateTime endDate, int? length)
+        {
+            var site = await _context.Sites.FindAsync(siteId);
+
+            var model = new CreateReservationViewModel
+            {
+                SiteID = siteId,
+                StartDate = startDate,
+                EndDate = endDate,
+                TrailerLengthFeet = length ?? 0
+                
+            };
+           
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Reserve(CreateReservationViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            // Get UserID from Session (more reliable with your current setup)
+            var userId = HttpContext.Session.GetInt32("UserID");
+            if (!userId.HasValue)
+                return Unauthorized();
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(c => c.UserID == userId.Value);
+
+            if (customer == null)
+                return Unauthorized();
+
+            var reservation = new Reservation
+            {
+                SiteID = model.SiteID,
+                CustomerID = customer.CustomerID,
+                ReservationStatusID = 1,
+                StartDate = model.StartDate,
+                EndDate = model.EndDate,
+                TrailerLengthFeet = model.TrailerLengthFeet,
+                NumberOfGuests = model.NumberOfGuests,
+                NumberOfPets = model.NumberOfPets,
+                Notes = model.SpecialRequests,
+                //needs to be update to have the correct pricing according to site and dates
+                BaseAmount = 0,
+                TotalAmount = 0,
+                BalanceDue = 0,
+                ScheduledCheckInTime = model.StartDate,
+                ScheduledCheckOutTime = model.EndDate,
+                DateCreated = DateTime.UtcNow,
+                LastUpdated = DateTime.UtcNow
+            };
+
+            _context.Reservations.Add(reservation);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("index", new { id = model.SiteID });
+        }
+
+
     }
 }
